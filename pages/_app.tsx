c560f648099ec284c756a388641b1c72f9d87f4e
Ian Gonzalez <ian.gonzalez@outlook.com.ar>
2022-09-11 20:44:16 -0300

    feat: use ProductContextContainer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import ProductContextContainer from '../contexts/ProductContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ProductContextContainer>
+    <Component {...pageProps} />
+  </ProductContextContainer>
 }
 
 export default MyApp
