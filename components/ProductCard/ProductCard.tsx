32c81fcb2b2013bad920766373b31cc425346842
Ian Gonzalez <ian.gonzalez@outlook.com.ar>
2022-09-12 14:16:28 -0300

    feat: use box component and fix add same product multiple times

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -1,18 +1,29 @@
 import React, { useContext } from 'react'
 import { ProductContext } from '../../contexts/ProductContext'
+import Box from '../Box'
 
 const ProductCard = ({ product: { id, name, price } }: any) => {
   const { cart, setCart } = useContext(ProductContext)
   return (
-    <div className='dark:bg-surface-dark bg-surface p-3 rounded-md flex flex-col gap-2'>
+    <Box>
       <p className='text-lg'>${price}</p>
       <p>{name}</p>
       <button className="rounded p-2 dark:bg-secondary-dark bg-secondary w-full" onClick={() => {
-        cart.push({ id, cant: 1, name, price });
+        console.log(cart);
+
+        const alreadyLoadedProduct = cart.find((product) => product.id == id)
+        console.log({ alreadyLoadedProduct });
+
+        if (alreadyLoadedProduct) {
+          alreadyLoadedProduct.cant++
+        }
+        else {
+          cart.push({ id, cant: 1, name, price });
+        }
         setCart(cart)
       }}>Agregar al carrito</button>
       <button className="rounded p-2 dark:bg-primary-dark bg-primary">Comprar</button>
-    </div>
+    </Box>
   )
 }
 
