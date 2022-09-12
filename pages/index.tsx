ea3ac9f89b01ce24fc512928085d560b9568098e
Ian Gonzalez <ian.gonzalez@outlook.com.ar>
2022-09-12 16:59:06 -0300

    feat: implement RigthDrawer

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
+import RigthDrawer from '../components/RigthDrawer'
 import CartList from '../components/CartList/CartList'
 import Modal from '../components/Modal'
 import ProductCard from '../components/ProductCard'
@@ -22,28 +23,25 @@ const Home: NextPage = () => {
         <ProductCard product={{ id: 1, name: 'TV', price: 1 }} />
         <ProductCard product={{ id: 2, name: 'iphone', price: .99 }} />
       </div>
-      {showCartList && (
-        <Modal title="Carrito" footer={
-          <>
-            <button
-              type="button"
-              className="inline-flex w-full justify-center rounded-md border border-transparent dark:bg-primary-dark bg-primary px-4 py-2 text-base font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-              onClick={() => router.push('/buy')}
-            >
-              Comprar
-            </button>
-            <button
-              type="button"
-              onClick={() => setShowCartList(false)}
-              className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 dark:bg-secondary-dark bg-secondary px-4 py-2 text-base font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-            >
-              Cancelar
-            </button>
-          </>
-        }>
+      <RigthDrawer isOpen={showCartList} onClose={setShowCartList} title="Carrito">
+        <>
           <CartList />
-        </Modal>
-      )}
+          <button
+            type="button"
+            className="inline-flex w-full justify-center rounded-md border border-transparent dark:bg-primary-dark bg-primary px-4 py-2 text-base font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+            onClick={() => router.push('/buy')}
+          >
+            Comprar
+          </button>
+          <button
+            type="button"
+            onClick={() => setShowCartList(false)}
+            className="inline-flex w-full justify-center rounded-md border border-gray-300 dark:bg-secondary-dark bg-secondary px-4 py-2 text-base font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+          >
+            Cancelar
+          </button>
+        </>
+      </RigthDrawer>
       <div className='w-full dark:bg-surface-dark bg-surface p-4 absolute bottom-0 left-0'>
         <button className='rounded p-2 dark:bg-primary-dark bg-primary ml-auto' onClick={() => setShowCartList(true)}>Finalizar compra</button>
       </div>
