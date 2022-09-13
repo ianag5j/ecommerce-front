dea76393df73225a3a29f587bf0832b2f4acfbc2
Ian Gonzalez <ian.gonzalez@outlook.com.ar>
2022-09-13 14:49:45 -0300

    feat: add loading spinner

--- pages/buy.tsx
+++ pages/buy.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react'
 import CartList from '../components/CartList/CartList'
+import LoadingSpinner from '../components/UI/LoadingSpinner'
 import { ProductContext } from '../contexts/ProductContext'
 import getTotalAmount from '../helpers/getTotalAmount'
 import { createOrder } from '../services/orders'
@@ -23,10 +24,7 @@ const Buy = () => {
           }
         }}>
           {!isLoading ? 'Pagar' : (
-            <svg className="animate-spin m-auto h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
+            <LoadingSpinner />
           )}
         </button>
       </div>
