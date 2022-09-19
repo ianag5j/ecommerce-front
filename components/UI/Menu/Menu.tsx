0052f0c823baaefb759ebc6ce0954f48598bef35
Ian Gonzalez <ian.gonzalez@outlook.com.ar>
2022-09-19 11:50:10 -0300

    feat: add ecommerce link to index

--- components/UI/Menu/Menu.tsx
+++ components/UI/Menu/Menu.tsx
@@ -3,13 +3,15 @@ import { Fragment } from 'react'
 import { ChevronDownIcon, UserIcon } from '@heroicons/react/20/solid'
 import Cookies from 'js-cookie'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 export default function Example() {
   const router = useRouter()
   const isLoged = Cookies.get('sess') !== undefined
   return (
-    <div className="fixed top-0 right-0 p-3 text-right dark:bg-surface-dark bg-surface w-full shadow h-[60px]">
-      <Menu as="div" className="relative inline-block text-left">
+    <div className="fixed top-0 right-0 p-3 dark:bg-surface-dark bg-surface w-full shadow h-[60px] flex items-center">
+      <Link href="/">Ecommerce</Link>
+      <Menu as="div" className="relative inline-block text-left ml-auto">
         <Menu.Button className="inline-flex w-full justify-center rounded-md dark:bg-primary-variant-dark bg-primary-variant px-4 py-2 text-sm font-medium text-white hover:bg-opacity-70 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
           Menu
           <ChevronDownIcon
@@ -30,28 +32,19 @@ export default function Example() {
           <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-surface dark:bg-surface-dark shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div className="px-1 py-1 ">
               {!isLoged && (
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      className={`${active ? 'bg-violet-500' : 'dark:text-white text-gray-900'
-                        } text-white group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      onClick={() => router.push('/login')}
-                    >
-                      {active ? (
-                        <UserIcon
-                          className="mr-2 h-5 w-5"
-                          aria-hidden="true"
-                        />
-                      ) : (
-                        <UserIcon
-                          className="mr-2 h-5 w-5"
-                          aria-hidden="true"
-                        />
-                      )}
-                      Iniciar sesion
-                    </button>
-                  )}
-                </Menu.Item>
+                <>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${active ? 'bg-violet-500' : 'dark:text-white text-gray-900'
+                          } text-white group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        onClick={() => router.push('/login')}
+                      >
+                        Iniciar sesion
+                      </button>
+                    )}
+                  </Menu.Item>
+                </>
               )}
               {isLoged && (
                 <>
