2c50283f58762a55b2d6246b6c97e9305e77ad94
Ian Gonzalez <ian.gonzalez@outlook.com.ar>
2022-11-22 23:13:17 -0300

    feat: use auth0 accessToken in get orders

--- pages/api/orders.ts
+++ pages/api/orders.ts
@@ -1,3 +1,4 @@
+import { getAccessToken } from '@auth0/nextjs-auth0'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getOrders } from '../../services/back/orders'
 
@@ -6,17 +7,18 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    if (!req.headers.authorization) {
+    const { accessToken } = await getAccessToken(req, res)
+    if (!accessToken) {
       return res.status(401).json({})
     }
     if (req.method !== 'GET') {
       return res.status(404).json({})
     }
 
-    const orders = await getOrders(req.headers.authorization)
+    const orders = await getOrders(accessToken)
     return res.status(200).json({ orders })
   } catch (error: any) {
-    console.log(error, error.response);
+    // console.log(error, error.response);
     res.status(500).json({ message: 'Error get orders' })
   }
 }
