42ce1801b0f2bd9283b62c22481df2eff4a5af61
Ian Gonzalez <ian.gonzalez@outlook.com.ar>
2022-11-24 17:42:41 -0300

    feat: add error message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import ProductContextContainer from '../contexts/ProductContext'
+import ErrorContextContainer from '../contexts/ErrorContext'
 import { UserProvider } from '@auth0/nextjs-auth0';
 import Layout from '../components/Layout'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <UserProvider>
     <ProductContextContainer>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ErrorContextContainer>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ErrorContextContainer>
     </ProductContextContainer>
   </UserProvider>
 )
