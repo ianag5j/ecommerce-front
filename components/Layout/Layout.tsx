949ec8b70d354ce780d0c38f75a9e2a5fee614da
Ian Gonzalez <ian.gonzalez@outlook.com.ar>
2022-12-01 12:08:37 -0300

    feat: add create order in a store

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,6 +1,4 @@
 import React, { useContext } from 'react'
-import Image from 'next/image'
-import styles from '../../styles/Home.module.css'
 import Menu from '../UI/Menu'
 import { useRouter } from 'next/router'
 import { ErrorContext } from '../../contexts/ErrorContext'
@@ -37,18 +35,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           </div>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </>
   )
 }
--- pages/store/[storeId]/buy.tsx
+++ pages/store/[storeId]/buy.tsx
@@ -5,10 +5,14 @@ import Button from 'components/UI/Button'
 import LoadingSpinner from 'components/UI/LoadingSpinner'
 import { ProductContext } from 'contexts/ProductContext'
 import { createOrder } from 'services/orders'
+import { useRouter } from 'next/router'
+import { ErrorContext } from 'contexts/ErrorContext'
 
 const Buy = () => {
   const { cart } = useContext(ProductContext)
   const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
+  const { setErrorMessage } = useContext(ErrorContext)
   return (
     <div className='flex flex-col gap-3 w-full'>
       <h2 className='text-xl mb-3'>Finalizar Compra</h2>
@@ -17,10 +21,10 @@ const Buy = () => {
         <Button disabled={isLoading} onClick={async () => {
           try {
             setIsLoading(true)
-            const order = await createOrder(cart);
-            window.location = order.links.checkoutLink;
-          } catch (error) {
-            alert('Error al crear la solicitud de pago');
+            const order = await createOrder(cart, router.query.storeId as string);
+            window.location = order.checkoutLink;
+          } catch (error: any) {
+            setErrorMessage(error.response.data.message)
             setIsLoading(false)
           }
         }}>
